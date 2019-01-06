d81e75d237620bd89c639199e4589f0d40b1aa9e
Debashish Deka <dekadebashish3@gmail.com>
2019-01-06 17:46:31 +0530

    kernel build setup

---
+++ kernelBuild.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get -y install bison
+sudo apt-get -y install flex
+sudo apt -y install tasksel
+sudo tasksel -y install openssh-server
+sudo apt -y install ssh
+sudo apt-get -y install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.xz
+tar -xvf linux-4.20.tar.xz
