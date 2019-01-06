fb6fe4fa3a8a4551a06624114505d241342f93dd
Debashish Deka <dekadebashish3@gmail.com>
2019-01-06 18:07:28 +0530

    updated

--- kernelBuild.sh
+++ kernelBuild.sh
@@ -12,3 +12,7 @@ tar -xvf linux-4.20.tar.xz
 cd linux-4.20
 cp /boot/config-$(uname -r) .config
 make menuconfig
+#sudo make -j 4 && sudo make modules_install -j 4 && sudo make install -j 4
+#update-initramfs -c -k 4.7.1   
+#update-grub  
+#sudo reboot
