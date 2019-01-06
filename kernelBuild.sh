2f195e18bfe1722c157be4dd66c707c9153e0880
Debashish Deka <dekadebashish3@gmail.com>
2019-01-06 18:06:21 +0530

    libelf-dev updated

--- kernelBuild.sh
+++ kernelBuild.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install flex
 sudo apt -y install tasksel
 sudo tasksel install openssh-server
 sudo apt -y install ssh
+sudo apt-get -y install libelf-dev
 sudo apt-get -y install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.xz
 tar -xvf linux-4.20.tar.xz
