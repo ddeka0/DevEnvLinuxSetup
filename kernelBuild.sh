e8c0b4643e2eafc2baf5c58dc60200b0b9ddfd3a
Debashish Deka <dekadebashish3@gmail.com>
2019-01-06 18:02:41 +0530

    updated

--- kernelBuild.sh
+++ kernelBuild.sh
@@ -3,8 +3,11 @@ sudo apt-get update
 sudo apt-get -y install bison
 sudo apt-get -y install flex
 sudo apt -y install tasksel
-sudo tasksel -y install openssh-server
+sudo tasksel install openssh-server
 sudo apt -y install ssh
 sudo apt-get -y install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.xz
 tar -xvf linux-4.20.tar.xz
+cd linux-4.20
+cp /boot/config-$(uname -r) .config
+make menuconfig
