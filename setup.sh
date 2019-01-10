54fd67aade6a4b6a5e1225a23466291c5afe4ec5
Debashish Deka <dekadebashish3@gmail.com>
2019-01-10 16:39:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,15 +18,15 @@ sudo apt-get install -y silversearcher-ag
 sudo apt-get install -y git
 
 #Zsh and Powerline configuration commands
-sudo apt-get install -y powerline fonts-powerline
-sudo apt-get install -y zsh-theme-powerlevel9k
-echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
-sudo apt-get install -y zsh-syntax-highlighting
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf\nwget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/
-sudo fc-cache -vf /usr/share/fonts/
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+#sudo apt-get install -y powerline fonts-powerline
+#sudo apt-get install -y zsh-theme-powerlevel9k
+#echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+#sudo apt-get install -y zsh-syntax-highlighting
+#echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf\nwget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+#sudo mv PowerlineSymbols.otf /usr/share/fonts/
+#sudo fc-cache -vf /usr/share/fonts/
+#sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 
 #Java8 install commands
@@ -37,4 +37,4 @@ sudo apt-get install -y oracle-java8-installer
 #colour picker tool install command
 # you can go to this website : http://mayccoll.github.io/Gogh/#0 and pick your fav color 
 # for terminator background
-sudo apt-get install -y gpick
+#sudo apt-get install -y gpick
